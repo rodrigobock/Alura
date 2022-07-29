a4cfea3fbc82bdff634ce3081f8a5d584ae81092
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-07-28 22:31:12 -0300

    Uso do Object.setPrototypeOf

--- JS-POO/Funcoes_e_Prototipos/objetoLiteral.js
+++ JS-POO/Funcoes_e_Prototipos/objetoLiteral.js
@@ -2,14 +2,32 @@ const user = {
     nome: 'Rodrigo',
     email: 'r@bock.com',
     nascimento: '2000/03/24',
-    role: 'admin',
+    role: 'estudante',
     ativo: true,
     exibirInfos: function(){
         console.log(this.nome, this.email)
     }
 }
 
-// Métodos 
+const admin = {
+    nome: 'Admin',
+    email: 'admin@admin.com',
+    nascimento: '2000/03/24',
+    role: 'admin',
+    ativo: true,
+    criarCurso: function(){
+        console.log('Curso criado')
+    }
+}
+
+// Passa o método exibirInfos do user pro admin
+Object.setPrototypeOf(admin, user)
+
+admin.criarCurso()
+admin.exibirInfos()
+
+
+/* Métodos 
 
 const exibir = function(){
     console.log(this.nome)
@@ -21,3 +39,5 @@ const exibirNome = exibir.bind(user) // -> bind permite que o this funcione, poi
 user.exibirInfos()
 
 exibirNome()
+
+*/
