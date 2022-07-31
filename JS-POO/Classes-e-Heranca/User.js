01f1f2d672b692c055e2142031151e565e99bf97
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-07-31 17:46:32 -0300

    getters and setters to user

--- JS-POO/Classes-e-Heranca/User.js
+++ JS-POO/Classes-e-Heranca/User.js
@@ -12,19 +12,50 @@ export default class User {
         this.#ativo = ativo
     }
 
-    #montaObjUser() {
-        return ({
-            nome: this.#nome,
-            email: this.#email,
-            nascimento: this.#nascimento,
-            role: this.#role,
-            ativo: this.#ativo
-        })
+    exibirInfos(){
+        return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}` 
     }
 
-    exibirInfos(){
-        const objUser = this.#montaObjUser()
-        return `${objUser.nome}, ${objUser.email}, ${objUser.nascimento}, ${objUser.role}, ${objUser.ativo}` 
+    // Getters and Setters
+
+    get nome() {
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+ 
+    get ativo() {
+        return this.#ativo
+    }
+
+    set nome(novoNome) {
+        this.#nome = novoNome
+    }
+
+    set email(novoEmail){
+        this.#email = novoEmail
+    }
+
+    set nascimento(novoNascimento){
+        this.#nascimento = novoNascimento
+    }
+
+    set role(novaRole){
+        this.#role = novaRole
+    }
+
+    set ativo(novoAtivo){
+        this.#ativo = novoAtivo
     }
 }
 
--- JS-POO/Classes-e-Heranca/index.js
+++ JS-POO/Classes-e-Heranca/index.js
@@ -2,8 +2,17 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User('Rodrigo', 'rodrigo@bock.com', '24/03/2000')
-console.log(novoUser.exibirInfos())
+// const novoUser = new User('Rodrigo', 'rodrigo@bock.com', '24/03/2000')
+// console.log(novoUser.exibirInfos())
 
 const novoAdmin = new Admin('Beatriz', 'beatriz@pellis.com', '23/05/2001')
-console.log(novoAdmin.exibirInfos())
+
+console.log(novoAdmin.nome)
+console.log(novoAdmin.email)
+console.log(novoAdmin.nascimento)
+console.log(novoAdmin.role)
+console.log(novoAdmin.ativo)
+
+novoAdmin.nome = "Rodrigo"
+
+console.log(novoAdmin.nome)
