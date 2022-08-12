2d88b51bd0c5fb6f0c371bc4379b445f2aa0d607
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-08-12 16:40:08 -0300

    Criando comunicação entre front e back para autenticação

--- Formação-Angular/GatitoBook/gatitobook/src/app/home/login/login.component.ts
+++ Formação-Angular/GatitoBook/gatitobook/src/app/home/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+usuario= '';
+senha= '';
+
+  constructor(private authService:AutenticacaoService) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+      this.authService.autenticar(this.usuario, this.senha).subscribe(() =>
+      {console.log('Autenticado com sucesso');},
+      (error) => {alert('Usuário ou senha invalido');
+      console.log(error)
+    })
   }
 
 }
