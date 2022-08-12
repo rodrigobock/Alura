1deb207371f2ed57d1789b00e415af39e3bcec78
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-08-12 18:01:52 -0300

    Validações personalizadas ao criar novo usuário

--- Formação-Angular/GatitoBook/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ Formação-Angular/GatitoBook/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,7 +1,8 @@
 import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
+import { minusculoValidator } from './minusculo.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -16,9 +17,13 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
-      userName: [''],
+      email: ['',[
+        Validators.required, Validators.email
+      ]],
+      fullName: ['',[
+        Validators.required, Validators.minLength(4)
+      ]],
+      userName: ['', minusculoValidator],
       password: ['']
     });
   }
