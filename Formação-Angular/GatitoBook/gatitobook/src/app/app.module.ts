01f0af71ef3ed49314184b27a0b5ca10b7067622
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-08-12 16:04:24 -0300

    Finalizando criação da tela de login

--- Formação-Angular/GatitoBook/gatitobook/src/app/app.module.ts
+++ Formação-Angular/GatitoBook/gatitobook/src/app/app.module.ts
@@ -12,8 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
