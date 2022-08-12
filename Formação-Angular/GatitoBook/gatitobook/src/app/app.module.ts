f29335b6df4ef7bd4c21d329f441a7b5ecefcab0
Rodrigo Cabral Bock <rodrigocabralbock@gmail.com>
2022-08-12 15:29:36 -0300

    Criando pagina Home

--- Formação-Angular/GatitoBook/gatitobook/src/app/app.module.ts
+++ Formação-Angular/GatitoBook/gatitobook/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
